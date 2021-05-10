44af014ac95d6574c78d5d98dccda2f341b284a6
Kirill Glushankov <kirantiq@gmail.com>
2021-05-10 04:18:58 +0300

    WebApp and pages

---
+++ Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebAppSTS.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppSTS.Startup))]
+namespace WebAppSTS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
