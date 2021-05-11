58b4192737f468a06d4ea4f9e377d8b791effb99
Kirill Glushankov <kirantiq@gmail.com>
2021-05-11 03:32:54 +0300

    add database

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity(1000,1), 
+    [Material] NVARCHAR(50) NOT NULL, 
+    [Quantity] INT NULL, 
+    [Cost] MONEY NULL
+)
---
+++ dbo.Table_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[object]
+(
+	[IdObj] INT NOT NULL PRIMARY KEY identity(1000,1), 
+    [ObjName] NVARCHAR(50) NULL, 
+    [Client] NVARCHAR(50) NULL, 
+    [Foreman] NVARCHAR(50) NULL, 
+    [DateBegin] DATE NULL, 
+    [Location] NVARCHAR(50) NULL, 
+    [Status] NVARCHAR(50) NULL
+)
